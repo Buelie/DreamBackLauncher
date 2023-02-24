ac19e19b4272dd343f63073287829bd0ca77e684
LYErikuy <111875719+Buelie@users.noreply.github.com>
2023-02-24 17:12:23 +0800

    Create Test.java

---
+++ DreamBackLauncher/log/Plugins/Test.java
@@ -0,0 +1,10 @@
+package log.Plugins;
+
+import log.PluginsAPI;
+
+public class Test {
+
+    public static void Test(){
+        PluginsAPI.LogData("This a test plugin(test.java)","Test","INFO");
+    }
+}
